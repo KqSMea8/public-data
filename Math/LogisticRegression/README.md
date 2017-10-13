d591f77aed07fa62d70f6f7e0de0113f16c02e94
gaotianpu <gaotianpu@gmail.com>
2017-10-13 13:23:31 +0800

    lr

--- Math/LogisticRegression/README.md
+++ Math/LogisticRegression/README.md
@@ -1,9 +1,4 @@
 # LogisticRegression
-Logistic regression from scratch in Python
-
-This example uses gradient descent to fit the model.
-It also contains a Scikit Learn's way of doing logistic regression, so we can compare the two implementations.
-
 
 https://chenrudan.github.io/blog/2016/01/09/logisticregression.html
 随着时间的增加，一个国家的大小（我理解为资源）和这个国家人们的生育能力限制了人口的增长，人口数量会渐渐趋近一个稳定值。
@@ -11,14 +6,19 @@ logisitc分布的累积分布函数
 一阶线性微分方程
 二阶微分方程
 
-https://github.com/perborgen/LogisticRegression
-
 
 https://lz5z.com/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0%E5%B8%B8%E7%94%A8%E7%AE%97%E6%B3%95%E2%80%94%E9%80%BB%E8%BE%91%E5%9B%9E%E5%BD%92/
+？
 
 http://www.cnblogs.com/sparkwen/p/3441197.html
 https://www.ibm.com/support/knowledgecenter/zh/SSLVMB_22.0.0/com.ibm.spss.statistics.help/spss/regression/idh_lreg.htm
 http://bluewhale.cc/2016-05-18/logistic-regression.html
 http://blog.csdn.net/zouxy09/article/details/20319673
 http://blog.sciencenet.cn/blog-508318-633085.html
+
+
+Logistic regression from scratch in Python
+
+This example uses gradient descent to fit the model.
+It also contains a Scikit Learn's way of doing logistic regression, so we can compare the two implementations.
 
---
+++ Math/LogisticRegression/lr.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+doc:
+https://lz5z.com/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0%E5%B8%B8%E7%94%A8%E7%AE%97%E6%B3%95%E2%80%94%E9%80%BB%E8%BE%91%E5%9B%9E%E5%BD%92/
+"""
+import os
+import sys
+import numpy as np
+# import sklearn as skl 
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+
+
+def wget(source_url, local_file=None):
+    """数据下载"""
+    if not local_file:
+        local_file = source_url.split('/')[-1]
+    # local_file = '/tmp/' + local_file
+    if not (os.path.isfile(local_file) and os.path.getsize(local_file) != 0):
+        os.system("wget '%s' -O %s" % (source_url, local_file))
+    return local_file
+
+
+def main():
+    # 加载数据
+    local_file = wget(
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+    )
+
+    # 把CSV文件转化为numpy matrix
+    dataset = np.loadtxt(local_file, delimiter=",")
+    # 训练集和结果
+    x = dataset[:, 0:7] #特征
+    y = dataset[:, 8] #label
+    # 数据归一化
+    normalized_x = preprocessing.normalize(x)
+    
+    # 逻辑回归
+    model = LogisticRegression() 
+    model.fit(normalized_x, y)
+
+    # 预测
+    expected = y
+    predicted = model.predict(normalized_x)
+
+    # 模型拟合概述
+    print(metrics.classification_report(expected, predicted))
+    print(metrics.confusion_matrix(expected, predicted))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+..... /2.7/Extras/lib/python/scipy/sparse/coo.py:200: 
+VisibleDeprecationWarning: `rank` is deprecated; 
+use the `ndim` attribute or function instead. 
+To find the rank of a matrix see `numpy.linalg.matrix_rank`.
+'''
